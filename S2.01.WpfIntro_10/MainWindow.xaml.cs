46abed4f15187126105611fdcdf8094001cc543a
sut <math323n@edu.campusvejle.dk>
2020-01-22 09:40:29 +0100

    df

--- S2.01.WpfIntro_10/MainWindow.xaml.cs
+++ S2.01.WpfIntro_10/MainWindow.xaml.cs
@@ -20,9 +20,25 @@ namespace S2._01.WpfIntro_10
     /// </summary>
     public partial class MainWindow: Window
     {
+        string[] validUsernames = new string[] { " Ole", "Brian" };
+        string[] validPasswords = new string[] { "MinHundErSød", "Lastbil2006$Rød" };
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Make login button visible when login is correct
+
+            if(validUsernames.Contains(textBoxUsername.Text) && validPasswords.Contains(textBoxPassword.Text))
+            {
+                if(hasLoggedIn.Visibility != System.Windows.Visibility.Hidden)
+                    hasLoggedIn.Visibility = System.Windows.Visibility.Hidden;
+                else
+                    hasLoggedIn.Visibility = System.Windows.Visibility.Visible;
+            }
         }
     }
 }
